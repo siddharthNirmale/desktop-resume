f1a4e7e5a0455c6cc7106d0addfb67e83755aac8
siddharthNirmale <siddharth175nirmale1@gmail.com>
2026-07-03 00:50:49 +0530

    fix: add line break for better readability in TerminalPortfolio component

--- src/mode/SmallDisplay.jsx
+++ src/mode/SmallDisplay.jsx
@@ -202,6 +202,7 @@ export default function TerminalPortfolio() {
                   <span className="relative flex h-1.5 w-1.5">
                     <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
                     <span className="relative inline-flex rounded-full h-1.5 w-1.5 bg-green-500"></span>
+                    <br />
                   </span>
                   Available for Opportunities
                 </span>
