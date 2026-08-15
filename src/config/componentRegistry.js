47ce1302b428f72df09dcd906f9b5d862da91637
siddharthNirmale <Siddharth175nirmale1@gmail.com>
2026-08-15 17:14:24 +0530

    fix: adjust GitHub widget position for improved layout

--- src/config/componentRegistry.js
+++ src/config/componentRegistry.js
@@ -53,7 +53,7 @@ export const WIDGET_PROPS = {
     positionStyle: { top: "250px", left: "18px" },
   },
   github: {
-    positionStyle: { top: "500px", left: "18px" },
+    positionStyle: { top: "501px", left: "18px" },
   },
   clock: {
     positionStyle: { top: "16px", right: "18px" },
